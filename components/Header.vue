4c74700513a9f92fc85f2afcda6e437082dca300
rowenslee <rowenslee@TL-ACER.(none)>
2022-11-23 14:08:52 +0700

    Fix link to homepage

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-dark-blue py-3 px-48">
-        <a href="/">
+        <a href="#home">
             <img src="~assets/logo.webp" width="150" height="150">
         </a>
     </div>
