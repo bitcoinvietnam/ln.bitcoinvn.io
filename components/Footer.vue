b55c795ef013eee9ded4478d37853b0899bd6452
rowenslee <rowenslee@TL-ACER.(none)>
2022-11-23 14:07:36 +0700

    Add open new tab to external link

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="bg-dark-blue text-white text-center py-14 px-48 text-gray-400">
         <p class="float-left text-sm hover:text-white">
-            © 2014 - {{ new Date().getFullYear() }} <a href="https://bitcoinvn.io" class="">
+            © 2014 - {{ new Date().getFullYear() }} <a href="https://bitcoinvn.io" target="_blank" class="">
                 BitcoinVN
             </a>
         </p>
         <p class="float-right">
             <span v-for="product in products" class="text-sm mx-2 hover:text-white">
-                <a :href=product.url>{{ product.caption }}</a>
+                <a :href=product.url target="_blank">{{ product.caption }}</a>
             </span>
             <!-- 
                 <span v-for="social in socials" class="text-sm">
