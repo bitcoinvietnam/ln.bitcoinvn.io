0620a3074b3f6c6ec3cbcb741a6df1901c17a560
rowenslee <rowenslee@TL-ACER.(none)>
2022-12-20 10:17:50 +0700

    Fix typo, add a link to bvn exchange

--- components/LightningInfo.vue
+++ components/LightningInfo.vue
@@ -7,7 +7,8 @@
                       What is the Lightning Network?
                     </div>
                     <div class="InfoText">
-                      <p>The most popular cryptocurrency in the world today is, of course, the oldest one, Bitcoin. But there is still the question, "Will Bitcoin be able to handle millions of daily transactions as more people adopt the cryptocurrency?".</p>
+                      <p>The most popular cryptocurrency in the world today is, of course, the oldest one, Bitcoin. But there is still the question: <span class="italic">
+                      "Will Bitcoin be able to handle millions of daily transactions as more people adopt the cryptocurrency?". </span></p>
                       <p>
                         The answer to this question might come in the form of the Lightning Network. The second layer technology for Bitcoin has the capability of boosting the number of transactions on the cryptocurrency per second while keeping the fees minuscule. For more information you can read a primer on the <a class="external-link" target="_blank" href="https://news.bitcoinvn.io/lightning-networks-advantages-as-payment-technology/"> advantages of the Lightning Network on BitcoinVN News</a>.
                       </p>
@@ -34,7 +35,7 @@
                     <div class="InfoText">
                       <p>The Lightning Network enhances the scalability and privacy of the Bitcoin Network while keeping the Bitcoin base layer unchanged.</p>
                       <p>
-                        Lightning Transactions settle generally within a few seconds maximum. You can see an example transaction in practice <a class="external-link" target="_blank" href="https://odysee.com/@FreedomVideos_and_more:2/tacos-in-vietnam-lightning-neutronpay:c">here</a>. You can also fund any <a class="external-link" target="_blank" href="https://bitcoinvn.io/?deposit=btcln&settle=vnd">Vietnamese</a> or <a class="external-link" target="_blank" href="https://bitcoinvn.io/?deposit=btcln&settle=rubbankcard">Russian</a> bank account via the services of BitcoinVN. Thats for the two boxes.
+                        Lightning Transactions settle generally within a few seconds maximum. You can see an example transaction in practice <a class="external-link" target="_blank" href="https://odysee.com/@FreedomVideos_and_more:2/tacos-in-vietnam-lightning-neutronpay:c">here</a>. You can also fund any <a class="external-link" target="_blank" href="https://bitcoinvn.io/?deposit=btcln&settle=vnd">Vietnamese</a> or <a class="external-link" target="_blank" href="https://bitcoinvn.io/?deposit=btcln&settle=rubbankcard">Russian</a> bank account via the services of <a class="external-link" target="_blank" href="https://bitcoinvn.io/">BitcoinVN</a>.
                       </p>
                       </div>
                     </div>
@@ -53,10 +54,10 @@
                         The Bitcoin Blockchain is on purpose relatively low throughput in order to avoid centralization pressures compromising the security of the network. As such it is not very useful as a payments network. Furthermore the audibility of the Bitcoin blockchain reveals and broadcasts any single transaction to the whole world.
                       </p>
                       <p>
-                        While this guarantees for no hidden monetary inflation, the Bitcoin Blockchain is not a very privacy-preserving tool for value transfers. The Lightning Network remedies these issues in part by Storing payment information only with the peers in the Lightning Network (the Bitcoin blockchain serves solely as settlement layer).
+                        While this guarantees for no hidden monetary inflation, the Bitcoin Blockchain is not a very privacy-preserving tool for value transfers. The Lightning Network remedies these issues in part by storing payment information only with the peers in the Lightning Network (the Bitcoin blockchain serves solely as settlement layer).
                       </p>
                       <p>
-                        Keeping payment information private between the connected peers in the Lightning Network (no public visibility), The Lightning Network therefore circumvents the issue that otherwise every single payment would be stored on the Bitcoin Blockchain (requiring huge bandwidth and storage), while avoiding to announce such payments to the whole world.
+                        Keeping payment information private between the connected peers in the Lightning Network (no public visibility), the Lightning Network therefore circumvents the issue that otherwise every single payment would be stored on the Bitcoin Blockchain (requiring huge bandwidth and storage), while avoiding to announce such payments to the whole world.
                       </p>
                 </div>
            </div>
