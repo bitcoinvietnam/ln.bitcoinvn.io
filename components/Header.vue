4bc16d04f42a267466d19e90432263db4c1e35c1
rowenslee <rowenslee@TL-ACER.(none)>
2023-02-16 16:02:26 +0700

    Refractor routerlink to nuxtlink tag

--- components/Header.vue
+++ components/Header.vue
@@ -2,15 +2,15 @@
     <div class="grid py-3 bg-dark-blue place-items-center md:place-items-start md:px-48">
         <div class="flex items-center align-middle flex-rows">
             <div>
-                <RouterLink to="/">
+                <NuxtLink to="/">
                     <img src="~assets/logo.webp" width="150" height="150" alt="BitcoinVN">
-                </RouterLink>
+                </NuxtLink>
             </div>
             <!--
-                        <div class="pt-1 text-xl font-black text-white">
-                            Lightning
-                        </div>
-                    -->
+                            <div class="pt-1 text-xl font-black text-white">
+                                Lightning
+                            </div>
+                        -->
         </div>
 </div>
 </template>
