83e244e96f7e0540840000a414decf4532a0ba64
rowenslee <rowenslee@TL-ACER.(none)>
2023-02-16 15:55:36 +0700

    Fix top header url

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="grid py-3 bg-dark-blue place-items-center md:place-items-start md:px-48">
-        <a href="#home">
-            <img src="~assets/logo.webp" width="150" height="150" alt="BitcoinVN">
-        </a>
-    </div>
+        <div class="flex items-center align-middle flex-rows">
+            <div>
+                <RouterLink to="/">
+                    <img src="~assets/logo.webp" width="150" height="150" alt="BitcoinVN">
+                </RouterLink>
+            </div>
+            <!--
+                        <div class="pt-1 text-xl font-black text-white">
+                            Lightning
+                        </div>
+                    -->
+        </div>
+</div>
 </template>
 
 <script>
+import { RouterLink } from 'vue-router'
+
 export default {
+    components: { RouterLink }
 }
 </script>
