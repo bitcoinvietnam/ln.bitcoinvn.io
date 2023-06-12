62b216a193d7dba86db2a37235ec74a6a6327d71
rowenslee <rowenslee@TL-ACER.(none)>
2023-06-12 15:58:58 +0700

    Fix gtag script

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,14 @@ export default {
     ],
     script: [
       { src: 'https://www.googletagmanager.com/gtag/js?id=G-G9RG2EHCEH', async: true },
-      { innerHTML: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date()); gtag('config', 'G-G9RG2EHCEH');", type: 'text/javascript', charset: 'utf-8' }
+      {
+        innerHTML: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){ dataLayer.push(arguments); };
+                    gtag('js', new Date());
+                    gtag('config', 'G-G9RG2EHCEH');`,
+        type: 'text/javascript',
+        charset: 'utf-8'
+      }
     ]
   },
 
